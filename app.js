b33f5bc361bc5008a90571cd5287f47907434b38
Yogesh Kumbhar <111835554+Yogesh188@users.noreply.github.com>
2025-03-28 11:56:33 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// App.js
+import NumberDictation from "./NumberDictation";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+      <NumberDictation />
+    </div>
+  );
+}
